21802f2aaf4b86f6f3f077c4ad9749672c4fe382
hitech chikan <hitechchikan@hitech-ka-Mac-mini.local>
2024-10-26 17:54:38 +0530

    mongodb

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 // database connection with mongoose
 
-mongoose.connect("mongodb+srv://virtugrowdigital:virtugrowdigital01@cluster0.gf08g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("mongodb://virtugrowdigital:virtugrowdigital01@cluster0-shard-00-00.gf08g.mongodb.net:27017,cluster0-shard-00-01.gf08g.mongodb.net:27017,cluster0-shard-00-02.gf08g.mongodb.net:27017/ava-chikan?ssl=true&replicaSet=atlas-fnj20a-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0");
 
 
 // api creation
@@ -20,11 +20,127 @@ mongoose.connect("mongodb+srv://virtugrowdigital:virtugrowdigital01@cluster0.gf0
 app.get('/',(req,res)=>{
     res.send("express app is running")
 })
+
+// image storage engine
+
+const storage=multer.diskStorage({
+    destination:'./upload/images',
+    filename:(req,file,cb)=>{
+     return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+
+    
+    }
+)
+
+
+const upload=multer({storage:storage})
+
+// creating upload endpoint for images
+
+app.use('/images',express.static('upload/images'))
+
+app.post("/upload",upload.single('product'),(req,res)=>{
+  res.json({
+    success:1,
+    image_url:`http://localhost:${port}/images/${req.file.filename}`
+  })
+})
+
+// creating schema
+
+const Product=mongoose.model("Product",{
+  id:{
+    type:Number,
+    require:true,
+  },
+  name:{
+    type:String,
+    required:true,
+  },
+  image:{
+    type:String,
+    required:true,
+  },
+  category:{
+    type:String,
+    required:true,
+  },
+  new_price:{
+    type:Number,
+    required:true,
+  },
+  old_price:{
+    type:Number,
+    required:true,
+  },
+  date:{
+    type:Date,
+    default:Date.now,
+  },
+  available:{
+    type:Boolean,
+    default:true,
+  },
+})
+
+// creating api for add product
+app.post('/removeproduct',async(req,res)=>{
+  await Product.findOneAndDelete({id:req.body.id});
+  console.log("removed");
+  res.json({
+    success:true,
+  name:req.body.name,
+  })
+})
+
+// creating api for get all products
+
+app.get('/allproducts',async(req,res)=>{
+  let products=await Product.find({});
+  console.log("all products fetched");
+  res.send(products)
+})
+
+
+app.post('/addproduct',async(req,res)=>{
+
+  let products=await Product.find({});
+  let id;
+  if(products.length>0){
+    let last_product_array=products.slice(-1);
+    let last_product=last_product_array[0];
+    id = last_product.id+1;
+  }else{
+    id=1; 
+  }
+
+  const product=new Product({
+    id:id,
+    name:req.body.name,
+    image:req.body.image,
+    category:req.body.category,
+    new_price:req.body.new_price,
+    old_price:req.body.old_price,
+
+      
+  });
+  console.log(product);
+  await product.save();
+  console.log("saved");
+  res.json({
+     success:true,
+     name:req.body.name, 
+  })
+})
+
+
+
 app.listen(port,(error)=>{
     if(!error) {
       console.log("server is running on port "+port )
     }
     else{
-        console.log("error:"+error)
+        console.log("error:" + error)
     }}
 )
